98d95766de258ec5abfa55cd5ce18e2383c77bb0
adamcnoel <DistantLight27@gmail.com>
2026-01-20 07:31:40 -0700

    Update prefixes-with-desc.js

--- prefixes-with-desc.js
+++ prefixes-with-desc.js
@@ -1,27 +1,101 @@
 const PREFIXES_WITH_DESC = {
-    "vita" : [ "life", "vitamin; vitality; vital; revitalize; viable; vitalism; devitalize" ],
-    "demo" : [ "people", "democracy; demography; undemocratic; democratize" ],
-    "stereo" : [ "solid", "stereoscope; stereophonic; stereotype; stereopticon; stereotropism" ],
-    "ism" : [ "doctrine", "Marxism; capitalism; Imagism; Cubism; nihilism; pluralism; tribalism" ],
-    "cogn" : [ "know", "recognize; cognizant; incognito; cognoscenti; cognomen; precognition" ],
-    "sur" : [ "over", "surplus; surpass; surcharge; surface; surfeit; surmount; surname; surtax" ],
-    "alter" : [ "other", "alternator; alteration; alter ego; alternative; altruism; altercation" ],
-    "astr" : [ "star", "astronomy; astrology; asteroid; disaster; asterisk; astrophysics; astrolabe" ],
-    "dyna" : [ "power", "dynamic; dynamo; dynamite; dynasty; dynamometer" ],
-    "chron" : [ "time", "chronometer; chronological; synchronize; chronic; anachronism" ],
-    "hyper" : [ "over", "hyperactive; hyperventilate; hyperbole; hyperacidity; hypertension" ],
-    "luna" : [ "moon", "lunar; lunatic; lunate; luna moth; lunette; sublunar; lunular" ],
-    "octa" : [ "eight", "octameter; octagenarian; octagon; octarchy; octave; octopus; octahedron" ],
-    "gyro" : [ "turn", "gyration; gyroscope; gyre; gyrate; spirogyra; gyromagnetic; gyrocompass" ],
-    "contra" : [ "against", "contradict; contrary; contrast; contrapuntal; contraband; contravene" ],
-    "geo" : [ "earth", "geography; geothermal; geology; geophysics; geometry; geosynchronous" ],
-    "helio" : [ "sun", "Helios; heliotropic; heliocentric; heliograph; perihelion; aphelion" ],
-    "thermo" : [ "heat", "thermostat; thermos; thermotropic; thermonuclear; thermocouple" ],
-    "tetra" : [ "four", "tetrameter; tetrahedron; tetroxide; tetragon; tetrachloride; tetracycline" ],
-    "meter" : [ "measure", "thermometer; millimeter; octameter; hydrometer; odometer" ],
-    "scope" : [ "look", "telescope; microscope; periscope; radarscope; horoscope; electroscope" ],
-    "son" : [ "sound", "sonar; unison; sonorous; sonnet; dissonance; resonant; supersonic" ],
-    "dec" : [ "ten", "decade; decaliter; decimal; decagon; decathlon; decimate; dodecahedron" ],
-    "stell" : [ "star", "interstellar; stelliform; stellar; constellation; stellate; stellify" ],
-    "amat" : [ "love", "amatory; amateur; amorous; amiable; amigo; amour-propre; amity" ]
+    vita : {
+        definition: "life",
+        examples: [ "vitamin", "vitality", "vital", "revitalize", "viable", "vitalism", "devitalize" ],
+    },
+    demo : {
+        definition: "people",
+        examples: [ "democracy", "demography", "undemocratic", "democratize" ],
+    },
+    stereo : {
+        definition: "solid",
+        examples: [ "stereoscope", "stereophonic", "stereotype", "stereopticon", "stereotropism" ],
+    },
+    ism : {
+        definition: "doctrine",
+        examples: [ "Marxism", "capitalism", "Imagism", "Cubism", "nihilism", "pluralism", "tribalism" ],
+    },
+    cogn : {
+        definition: "know",
+        examples: [ "recognize", "cognizant", "incognito", "cognoscenti", "cognomen", "precognition" ],
+    },
+    sur : {
+        definition: "over",
+        examples: [ "surplus", "surpass", "surcharge", "surface", "surfeit", "surmount", "surname", "surtax" ],
+    },
+    alter : {
+        definition: "other",
+        examples: [ "alternator", "alteration", "alter ego", "alternative", "altruism", "altercation" ],
+    },
+    astr : {
+        definition: "star",
+        examples: [ "astronomy", "astrology", "asteroid", "disaster", "asterisk", "astrophysics", "astrolabe" ],
+    },
+    dyna : {
+        definition: "power",
+        examples: [ "dynamic", "dynamo", "dynamite", "dynasty", "dynamometer" ],
+    },
+    chron : {
+        definition: "time",
+        examples: [ "chronometer", "chronological", "synchronize", "chronic", "anachronism" ],
+    },
+    hyper : {
+        definition: "over",
+        examples: [ "hyperactive", "hyperventilate", "hyperbole", "hyperacidity", "hypertension" ],
+    },
+    luna : {
+        definition: "moon",
+        examples: [ "lunar", "lunatic", "lunate", "luna moth", "lunette", "sublunar", "lunular" ],
+    },
+    octa : {
+        definition: "eight",
+        examples: [ "octameter", "octagenarian", "octagon", "octarchy", "octave", "octopus", "octahedron" ],
+    },
+    gyro : {
+        definition: "turn",
+        examples: [ "gyration", "gyroscope", "gyre", "gyrate", "spirogyra", "gyromagnetic", "gyrocompass" ],
+    },
+    contra : {
+        definition: "against",
+        examples: [ "contradict", "contrary", "contrast", "contrapuntal", "contraband", "contravene" ],
+    },
+    geo : {
+        definition: "earth",
+        examples: [ "geography", "geothermal", "geology", "geophysics", "geometry", "geosynchronous" ],
+    },
+    helio : {
+        definition: "sun",
+        examples: [ "Helios", "heliotropic", "heliocentric", "heliograph", "perihelion", "aphelion" ],
+    },
+    thermo : {
+        definition: "heat",
+        examples: [ "thermostat", "thermos", "thermotropic", "thermonuclear", "thermocouple" ],
+    },
+    tetra : {
+        definition: "four",
+        examples: [ "tetrameter", "tetrahedron", "tetroxide", "tetragon", "tetrachloride", "tetracycline" ],
+    },
+    meter : {
+        definition: "measure",
+        examples: [ "thermometer", "millimeter", "octameter", "hydrometer", "odometer" ],
+    },
+    scope : {
+        definition: "look",
+        examples: [ "telescope", "microscope", "periscope", "radarscope", "horoscope", "electroscope" ],
+    },
+    son : {
+        definition: "sound",
+        examples: [ "sonar", "unison", "sonorous", "sonnet", "dissonance", "resonant", "supersonic" ],
+    },
+    dec : {
+        definition: "ten",
+        examples: [ "decade", "decaliter", "decimal", "decagon", "decathlon", "decimate", "dodecahedron" ],
+    },
+    stell : {
+        definition: "star",
+        examples: [ "interstellar", "stelliform", "stellar", "constellation", "stellate", "stellify" ],
+    },
+    amat : {
+        definition: "love",
+        examples: [ "amatory", "amateur", "amorous", "amiable", "amigo", "amour-propre", "amity" ]
 };
